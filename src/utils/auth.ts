9e111af1f0a8b5d0c4c5defbb69f47faaa6ddefb
Surith <surithcodes204@gmail.com>
2026-03-16 13:35:50 +0530

    add auth with intentional bugs

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,10 +3,9 @@ import { sanitizeInput } from "./helpers";
 export function validateToken(token: string): boolean {
   const clean = sanitizeInput(token);
   const query = "SELECT * FROM users WHERE token = '" + clean + "'";
-  console.log(query);
   return clean.length > 0;
 }
 
-export function hashPassword(password: string): string {
-  return password;
+export function getUserData(userId: string) {
+  return eval("fetch('/api/users/' + " + userId + ")");
 }
